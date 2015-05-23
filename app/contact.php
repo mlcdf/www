1af2a5bfb9c0c6fe66ca7600bdcb6587cef69cef
Maxime LeContedesFloris <maxime.lcdf@gmail.com>
2015-05-23 13:05:48 +0200

    0.5

--- app/contact.php
+++ app/contact.php
@@ -1,19 +1,30 @@
 <?php
 
-if ($_POST["submit"]) {
+
+if(!empty($_POST['name']) && !empty($_POST['email']))
 {
     $to = "maxime.lcdf@gmail.com";
     $name = $_POST['name'];
     $email = $_POST['email'];
+    $subject = "Contact website";
     $message = $_POST['message'];
+
+    $headers ='From: contact@maximelecontedesfloris.com'."\n";
+    $headers .='Reply-To: contact@maximelecontedesfloris.com'."\n";
+    $headers .='Content-Type: text/plain; charset="iso-8859-1"'."\n";
+    $headers .='Content-Transfer-Encoding: 8bit';
 
     $body = "De: $name\n E-Mail: $email\n Message:\n $message";
 
-    mail($to, $body);
+    mail($to, $subject, $body, $headers);
 
+    echo '<script language="javascript">alert("Message successfully sent.")</script>';
+    header( "refresh:2;url=http://maximelecontedesfloris.com/" );
 
-    header('Location: index.html');
-    echo "ça marche";
-
+}
+else
+{
+  echo '<script language="javascript">alert("Something went wrong. Please try again.")</script>';
+  header( "refresh:5;url=http://maximelecontedesfloris.com/" );
 }
 ?>
