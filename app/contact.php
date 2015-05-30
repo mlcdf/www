e85889fab84a70466096f1a836110d3af9d16472
Maxime LeContedesFloris <maxime.lcdf@gmail.com>
2015-05-30 15:21:43 +0200

    New font for titles

--- app/contact.php
+++ app/contact.php
@@ -1,8 +1,5 @@
 <?php
 
-
-if(!empty($_POST['name']) && !empty($_POST['email']))
-{
     $to = "maxime.lcdf@gmail.com";
     $name = $_POST['name'];
     $email = $_POST['email'];
@@ -14,17 +11,9 @@ if(!empty($_POST['name']) && !empty($_POST['email']))
     $headers .='Content-Type: text/plain; charset="iso-8859-1"'."\n";
     $headers .='Content-Transfer-Encoding: 8bit';
 
-    $body = "De: $name\n E-Mail: $email\n Message:\n $message";
+    $body = "De: $name\nE-Mail: $email\nMessage:\n $message";
 
     mail($to, $subject, $body, $headers);
 
-    echo '<script language="javascript">alert("Message successfully sent.")</script>';
-    header( "refresh:2;url=http://maximelecontedesfloris.com/" );
 
-}
-else
-{
-  echo '<script language="javascript">alert("Something went wrong. Please try again.")</script>';
-  header( "refresh:5;url=http://maximelecontedesfloris.com/" );
-}
 ?>
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log("Hey there !");
+$(document).ready(function() {
+
+  'use strict';
+  console.log('Hey there !');
+
+  $('form').submit(function() {
+
+    event.preventDefault();
+
+    $.ajax({
+      type: 'POST',
+      url: 'contact.php',
+      data: $('form').serialize(),
+      success: function() {
+        $('#confirmation').text('Message successfully sent. Thank you !');
+      },
+      error: function() {
+        $('#confirmation').text('Something went wrong. Please try again.');
+      }
+    });
+  });
+
+});
