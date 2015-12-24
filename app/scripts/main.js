65db35e4e20e86fc94dfdebb24b14ca09b1223e9
Maxime Le Conte des Floris <maxime.lcdf@gmail.com>
2015-12-24 17:25:20 +0100

    from 4 to 6 load-animations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,11 @@
 'use strict';
 
 window.onload = function(){
-    document.getElementById('one').setAttribute('class', 'row load-animation-1');
-    document.getElementById('two').setAttribute('class', 'row load-animation-2');
-    document.getElementById('three').setAttribute('class', 'row load-animation-3');
-    document.getElementById('four').setAttribute('class', 'row load-animation-4');
+    document.getElementById('one').setAttribute('class', 'column-half  load-animation-1');
+    document.getElementById('two').setAttribute('class', 'column-half  load-animation-2');
+    document.getElementById('three').setAttribute('class', 'column-half  load-animation-3');
+    document.getElementById('four').setAttribute('class', 'column-half  load-animation-4');
+    document.getElementById('five').setAttribute('class', 'column-half  load-animation-5');
+    document.getElementById('six').setAttribute('class', 'column-half  load-animation-6');
+    document.getElementById('seven').setAttribute('class', 'row  load-animation-7');
 };
