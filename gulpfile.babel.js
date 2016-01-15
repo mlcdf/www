9cf9539b9db540097bfab6fb509fc17ec785ecd4
mlcdf <maxime.lcdf@gmail.com>
2016-01-15 16:15:34 +0100

    design tweaks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -96,7 +96,8 @@ gulp.task('serve', ['styles', 'fonts'], () => {
     server: {
       baseDir: ['.tmp', 'app'],
       routes: {
-        '/bower_components': 'bower_components'
+        '/bower_components': 'bower_components',
+        '/node_modules': 'node_modules'
       }
     }
   });
