1f38103f2671cefa1d6cfd419f6e6f504941bdba
Maxime Le Conte des Floris <maxime.lcdf@gmail.com>
2016-09-14 15:39:14 +0200

    fix link to manifest and css

--- src/components/Head.js
+++ src/components/Head.js
@@ -11,8 +11,8 @@ export default class Head extends React.Component {
               <meta name="author" content="Maxime Le Conte des Floris"/>
               <meta name="theme-color" content="#333"/>
               <title>{this.props.title}</title>
-              <link rel="manifest" href="manifest.json"/>
-              <link rel="stylesheet" href="styles.css"/>
+              <link rel="manifest" href="/manifest.json"/>
+              <link rel="stylesheet" href="/styles.css"/>
             </head>
         )
     }
