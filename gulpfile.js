167951bb060d4b671ba69f1a66288bacab1d51de
Maxime Le Conte des Floris <maxime.lcdf@gmail.com>
2017-06-17 18:08:37 +0200

    gulp: update paths, rename tasks and add nodemon task

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const cssnano = require('gulp-cssnano')
 const gulp = require('gulp')
 const gulpif = require('gulp-if')
 const imagemin = require('gulp-imagemin')
+const nodemon = require('gulp-nodemon')
 const plumber = require('gulp-plumber')
 const pngcrush = require('imagemin-pngcrush')
 const sass = require('gulp-sass')
@@ -23,19 +24,19 @@ gulp.task('minify-svg', () => {
 
 gulp.task('minify-img', () => {
   gulp
-    .src('./img/*')
-    .pipe(size({ gzip: false, showFiles: true, title: 'original image size' }))
-    .pipe(size({ gzip: true, showFiles: true, title: 'original image size' }))
+    .src('./public/images/*')
+    .pipe(size({gzip: false, showFiles: true, title: 'original image size'}))
+    .pipe(size({gzip: true, showFiles: true, title: 'original image size'}))
     .pipe(
       imagemin({
         progressive: true,
-        svgoPlugins: [{ removeViewBox: false }],
+        svgoPlugins: [{removeViewBox: false}],
         use: [pngcrush()]
       })
     )
-    .pipe(size({ gzip: false, showFiles: true, title: 'minified images' }))
-    .pipe(size({ gzip: true, showFiles: true, title: 'minified images' }))
-    .pipe(gulp.dest('./img')) // change the dest if you don't want your images overwritten
+    .pipe(size({gzip: false, showFiles: true, title: 'minified images'}))
+    .pipe(size({gzip: true, showFiles: true, title: 'minified images'}))
+    .pipe(gulp.dest('./public/images/')) // Change the dest if you don't want your images overwritten
 })
 
 // Task that compiles scss files down to good old css
@@ -51,7 +52,7 @@ gulp.task('sass', () => {
       })
     )
     .pipe(gulp.dest('./public/styles/'))
-    .pipe(browserSync.reload({ stream: true }))
+    .pipe(browserSync.reload({stream: true}))
 })
 
 gulp.task('es6', () => {
@@ -61,7 +62,7 @@ gulp.task('es6', () => {
     .pipe(babel())
     .pipe(concat('bundle.js'))
     .pipe(gulp.dest('./public/scripts/'))
-    .pipe(browserSync.reload({ stream: true }))
+    .pipe(browserSync.reload({stream: true}))
 })
 
 gulp.task('generate-service-worker', callback => {
@@ -83,8 +84,7 @@ gulp.task('generate-service-worker', callback => {
           handler: 'cacheFirst',
           urlPattern: /^https:\/\/localhost/
         }
-      ],
-      navigateFallback: '/offline'
+      ]
     },
     callback
   )
@@ -97,16 +97,30 @@ gulp.task('watch', () => {
   gulp.watch('public/**/*.scss', ['sass', 'generate-service-worker'])
 })
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', ['nodemon'], () => {
   browserSync.init(null, {
-    proxy: 'https://localhost:3000',
+    proxy: 'http://localhost:3000',
     files: ['public/**/*.*'],
     browser: 'firefox',
     port: 7000
   })
 })
 
-gulp.task('default', [
+gulp.task('nodemon', cb => {
+  let started = false
+
+  return nodemon({
+    script: './bin/www'
+  }).on('start', () => {
+    // To avoid nodemon being started multiple times
+    if (!started) {
+      cb()
+      started = true
+    }
+  })
+})
+
+gulp.task('serve', [
   'generate-service-worker',
   'es6',
   'sass',
@@ -115,7 +129,7 @@ gulp.task('default', [
 ])
 
 gulp.task(
-  'production',
+  'build',
   ['sass', 'es6', 'generate-service-worker', 'minify-img', 'minify-svg'],
   () => {
     return gulp
@@ -127,7 +141,7 @@ gulp.task(
           cssnano({
             safe: true,
             autoprefixer: false,
-            discardComments: { removeAll: true }
+            discardComments: {removeAll: true}
           })
         )
       )
