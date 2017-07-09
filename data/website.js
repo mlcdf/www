b9d6b1b8bb1657b2809b4809deabdf7c1a962945
Maxime Le Conte des Floris <maxime.lcdf@gmail.com>
2017-07-09 20:20:58 +0200

    projects: update design + add new projects

--- data/website.js
+++ data/website.js
@@ -14,54 +14,5 @@ module.exports = {
   description:
     'Maxime Le Conte des Floris is a web developer and free software & open source/data enthusiast. He\'s passionate about performance and UX on the web.',
   source: 'https://github.com/mlcdf/mlcdf',
-  lastUpdate: '',
-  included: {
-    projects: [
-      {
-        name: 'cook',
-        description:
-          'Easily install popular softwares that are missing from APT.',
-        url: 'https://github.com/cook-pkg/cook',
-        img: '1f4e6.svg',
-        alt: 'package emoji',
-        color: '83dcff'
-      },
-      {
-        name: 'ikarauraga',
-        description:
-          '2D space shooter made with Unity for Ludum Dare 35 with Clément Stéphano.',
-        url: 'https://github.com/mlcdf/ikarauraga',
-        img: '1f3ae.svg',
-        alt: 'video game controller emoji',
-        color: 'f51843'
-      },
-      {
-        name: 'dotfiles',
-        description:
-          'My config files: zsh, oh-my-zsh, z, git, node.js, python, atom, hyper, ...',
-        url: 'https://github.com/mlcdf/dotfiles',
-        img: '1f527.svg',
-        alt: 'wrench emoji',
-        color: 'ffeb3b'
-      },
-      {
-        name: 'generator-pym',
-        description:
-          'Scaffold out a Python module. Includes CI, license, Makefile, pylint, ...',
-        url: 'https://github.com/mlcdf/generator-pym',
-        img: '1f40d.svg',
-        alt: 'snake emoji',
-        color: 'e9e9e9'
-      },
-      {
-        name: 'material-utilities',
-        description:
-          'Set of CSS classes & custom properties for Material shadows and opacities.',
-        url: 'https://github.com/mlcdf/material-utilities',
-        img: '2728.svg',
-        alt: 'sparkle emoji',
-        color: '000000'
-      }
-    ]
-  }
+  lastUpdate: ''
 }
