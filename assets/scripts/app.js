8839477f71dd7f05baeea725cb6ea5a094c7a628
Maxime Le Conte des Floris <maxime.lcdf@gmail.com>
2017-08-17 14:57:39 +0200

    fix wrong path to sw.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,7 +12,7 @@ if ('serviceWorker' in navigator) {
   // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
   // See https://github.com/slightlyoff/ServiceWorker/issues/468
   navigator.serviceWorker
-    .register('./sw.js')
+    .register('/sw.js')
     .then(reg => {
       // updatefound is fired if service-worker.js changes.
       reg.onupdatefound = function() {
