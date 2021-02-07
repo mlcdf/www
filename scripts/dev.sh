b775bf1742457cf7e0c61c475356466f91ccc5cc
Maxime Le Conte des Floris <maxime.lcdf@gmail.com>
2021-02-07 16:06:36 +0100

    decrypt assets in dev.sh

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+python -m crypto decrypt theme/static/images/leaves.svg --key key.key
+python -m crypto decrypt theme/static/images/rss.svg --key key.key
+python -m crypto decrypt theme/static/images/code-website.svg --key key.key
 pelican --settings pelicanconf.py  --listen --autoreload
