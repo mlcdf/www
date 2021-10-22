d096fe1c7b29d2f7368c0d0bb68ca2a9d329cd0a
Maxime Le Conte des Floris <git@mlcdf.fr>
2021-10-22 20:24:48 +0200

    set default arg dev.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -eu
 
+if [ $# -eq 0 ]
+  then
+    echo "\e[1;33mNo arguments supplied. Continuing with dev.sh\e[0m"
+fi
+
+SCRIPT=${1:-dev.sh}
+
 docker build -t pelican .
 docker run \
     -p 8000:8000 \
@@ -13,4 +20,4 @@ docker run \
     -v $(pwd)/publishconf.py:/website/publishconf.py \
     -v $(pwd)/key.key:/website/key.key \
     --rm \
-    -it pelican ./scripts/$@
+    -it pelican ./scripts/${SCRIPT}
