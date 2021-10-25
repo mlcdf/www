a0482958deb8970d0ef9e45ae65a782cce8ace96
Maxime Le Conte des Floris <git@mlcdf.fr>
2021-10-25 22:24:31 +0200

    fix port in local-preview

--- scripts/local-preview.sh
+++ scripts/local-preview.sh
@@ -3,6 +3,6 @@ set -eu
 
 $(dirname "$0")/docker.sh publish.sh
 
-xdg-open http://www.mlcdf.local:8080
+xdg-open http://www.mlcdf.local:8000
 
-docker run -p 8080:8080 -v $(pwd)/output:/var/www:ro  --rm syntaqx/serve
+docker run -p 8000:8080 -v $(pwd)/output:/var/www:ro  --rm syntaqx/serve
