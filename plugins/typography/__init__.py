1c9bcc7707d0bbccad30ef7773237ed6ff8ca8f3
Maxime Le Conte des Floris <git@mlcdf.fr>
2022-05-21 00:28:15 +0200

    use typographeur in pages too

--- plugins/typography/__init__.py
+++ plugins/typography/__init__.py
@@ -1,18 +1,20 @@
-import datetime
-
 from pelican import signals
-from pelican.contents import Article
-from pelican.readers import BaseReader
 
 from typographeur import typographeur
 
 
-def addArticle(articleGenerator):
+def apply_rules_for_articles(articleGenerator):
     settings = articleGenerator.settings
 
     for article in articleGenerator.articles:
         article._content = typographeur(article._content, fix_semicolon=False)
 
 
+def apply_rules_for_pages(pageGenerator):
+    for page in pageGenerator.pages:
+        page._content = typographeur(page._content, fix_semicolon=False)
+
+
 def register():
-    signals.article_generator_finalized.connect(addArticle)
+    signals.article_generator_finalized.connect(apply_rules_for_articles)
+    signals.page_generator_finalized.connect(apply_rules_for_pages)
